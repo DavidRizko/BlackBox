b2dc83ede282daf6ff31566408540f639562c45a
DavidRizko <david@imbed.net>
2024-06-23 13:53:28 -0700

    Initial Commit

---
+++ BlackBox/BlackBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackBoxApp.swift
+//  BlackBox
+//
+//  Created by David Rizko on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct BlackBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlackBoxUITests/BlackBoxUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BlackBoxUITestsLaunchTests.swift
+//  BlackBoxUITests
+//
+//  Created by David Rizko on 6/23/24.
+//
+
+import XCTest
+
+final class BlackBoxUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
